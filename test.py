db0d3b60ef42b94bafaa3c77f3fba72b20be4161
tmyszak <tomasz.m.myszak@gmail.com>
2016-10-04 18:38:22 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,48 @@
+'''
+skrypt1
+'''
+
+a = 45
+print a
+b = 54
+napis = "asdasd"
+
+print str(b) + " " + napis
+
+a, b, c = 4, 8, 9
+print a, b, c
+
+del a
+
+a = 0O15
+print a
+
+a = 0xabc
+print a
+
+a = 0b10101010110101111111
+print a
+
+a = 3 + 5j
+print a
+
+a = 4.6
+print a
+
+napis = "To jest\" String"
+print napis
+
+napis = "Test z tabulatorem\ti znakiem\n nowego wiersza"
+print napis
+
+napis = '''napis
+o wielu
+wierszach
+'''
+print napis
+
+print "zielone " + "jablko"
+
+print "B" + "a" *5+"rdzo pyszne"
+
+print "Py" "thon"
